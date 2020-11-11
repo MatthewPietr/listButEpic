90652417a73237ff8b466ba416e7231314a61130
MatthewPietr <mxp18b@acu.edu>
2020-11-11 17:32:52 -0600

    Initial Commit

---
+++ listButEpic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  listButEpic
+//
+//  Created by Matthew Pietrucha on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ listButEpic/listButEpicApp.swift
@@ -0,0 +1,17 @@
+//
+//  listButEpicApp.swift
+//  listButEpic
+//
+//  Created by Matthew Pietrucha on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct listButEpicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
